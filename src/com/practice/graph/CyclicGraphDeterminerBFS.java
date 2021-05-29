58f53eb5d7384e3cedfeee314b5e9bef100dcee1
Sharnendra <sharnendradey@gmail.com>
2021-05-30 00:49:13 +0530

    changes in the class

--- src/com/practice/graph/CyclicGraphDeterminerBFS.java
+++ src/com/practice/graph/CyclicGraphDeterminerBFS.java
@@ -31,69 +31,62 @@ public class CyclicGraphDeterminerBFS {
 		addEdge(adj, 0, 4);
 		addEdge(adj, 1, 2);
 		addEdge(adj, 1, 3);
-		//addEdge(adj, 1, 4);
+		// addEdge(adj, 1, 4);
 		addEdge(adj, 2, 3);
-		//addEdge(adj, 3, 4);
-		//addEdge(adj, 4, 0);
-		System.out.println(BFS(adj,0 , 5));
+		// addEdge(adj, 3, 4);
+		// addEdge(adj, 4, 0);
+		System.out.println(BFS(adj, 0, 5));
 	}
-	
+
 	// Perform BFS on the graph starting from vertex `src` and
-    // return true if a cycle is found in the graph
-    public static boolean BFS(List<ArrayList<Integer>> graph, int src, int N)
-    {
-        // to keep track of whether a vertex is discovered or not
-        boolean[] discovered = new boolean[N];
- 
-        // mark the source vertex as discovered
-        discovered[src] = true;
- 
-        // create a queue for doing BFS and
-        // enqueue source vertex
-        Queue<Node> q = new ArrayDeque<>();
-        q.add(new Node(src, -1));
- 
-        // loop till queue is empty
-        while (!q.isEmpty())
-        {
-            // dequeue front node and print it
-            Node node = q.poll();
- 
-            // do for every edge `v —> u`
-            for (int u: graph.get(node.v))
-            {
-                if (!discovered[u])
-                {
-                    // mark it as discovered
-                    discovered[u] = true;
- 
-                    // construct the queue node containing info
-                    // about vertex and enqueue it
-                    q.add(new Node(u, node.v));
-                }
- 
-                // `u` is discovered, and `u` is not a parent
-                else if (u != node.parent)
-                {
-                    // we found a cross-edge, i.e., the cycle is found
-                    return true;
-                }
-            }
-        }
- 
-        // no cross-edges were found in the graph
-        return false;
-    }
+	// return true if a cycle is found in the graph
+	public static boolean BFS(List<ArrayList<Integer>> graph, int src, int N) {
+		// to keep track of whether a vertex is discovered or not
+		boolean[] discovered = new boolean[N];
+
+		// mark the source vertex as discovered
+		discovered[src] = true;
+
+		// create a queue for doing BFS and
+		// enqueue source vertex
+		Queue<Node> queue = new ArrayDeque<>();
+		queue.add(new Node(src, -1));
+
+		// loop till queue is empty
+		while (!queue.isEmpty()) {
+			// dequeue front node and print it
+			Node node = queue.poll();
+
+			// do for every edge `v —> u`
+			for (int u : graph.get(node.value)) {
+				if (!discovered[u]) {
+					// mark it as discovered
+					discovered[u] = true;
+
+					// construct the queue node containing info
+					// about vertex and enqueue it
+					queue.add(new Node(u, node.value));
+				}
+
+				// `u` is discovered, and `u` is not a parent
+				else if (u != node.parent) {
+					// we found a cross-edge, i.e., the cycle is found
+					return true;
+				}
+			}
+		}
+
+		// no cross-edges were found in the graph
+		return false;
+	}
 }
 
 //Node to store vertex and its parent info in BFS
-class Node
-{
-    int v, parent;
- 
-    Node(int v, int parent)
-    {
-        this.v = v;
-        this.parent = parent;
-    }
+class Node {
+	int value, parent;
+
+	Node(int value, int parent) {
+		this.value = value;
+		this.parent = parent;
+	}
 }
